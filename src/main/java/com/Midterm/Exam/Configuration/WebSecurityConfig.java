c255c4df9204b3751eca4734f5cf6e31d5485dee
IhsanGokalp <ihsangokalp01@hotmail.com>
2020-04-03 15:31:19 +0600

    configuration

--- src/main/java/com/Midterm/Exam/Configuration/WebSecurityConfig.java
+++ src/main/java/com/Midterm/Exam/Configuration/WebSecurityConfig.java
@@ -22,7 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
         .and()
             .formLogin()
-                .loginPage("/login").permitAll();
+                .loginPage("/login").permitAll()
+        ;
     }
 
     @Bean
