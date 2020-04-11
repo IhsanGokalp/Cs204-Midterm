70e59a76261bd16c230518e4fc3ef1c08ff9e9c9
IhsanGokalp <ihsangokalp01@hotmail.com>
2020-04-11 16:22:01 +0600

    Deploying to server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/Exam-0.0.1-SNAPSHOT.jar
--- src/main/java/com/Midterm/Exam/Configuration/WebSecurityConfig.java
+++ src/main/java/com/Midterm/Exam/Configuration/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.Midterm.Exam.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,12 +20,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/home","/customers").permitAll()
+                .antMatchers("/", "/home","/customers").permitAll()
                 .anyRequest().authenticated()
         .and()
-            .formLogin()
-                .loginPage("/login").permitAll()
-        ;
+            .csrf().disable();
     }
 
     @Bean
